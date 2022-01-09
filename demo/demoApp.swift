b32f56f5902050080893eaf8681aa2b8972fda28
MeryPopins312 <meka.toktogulova@gmail.com>
2022-01-09 13:35:46 +0600

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Mery Popins on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
